f6be306f34cb4c287f53f87748c145b7c42333b6
haktan-tekin <webhaktan@gmail.com>
2023-05-10 21:54:29 +0300

    convert: mantine component

--- components/main/header/Index.tsx
+++ components/main/header/Index.tsx
@@ -1,20 +1,18 @@
-import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
-import SearchIcon from '@mui/icons-material/Search';
-import AddIcon from '@mui/icons-material/Add';
-import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
 import Image from "next/image";
 import Link from "next/link";
 import { useState } from 'react';
 import ShowProfile from './ShowProfile';
+import { IconCameraSearch, IconBellRinging, IconBrandHipchat, IconPlus } from '@tabler/icons-react';
+import { Menu, Button } from '@mantine/core';
 
 export default function Header() {
   const [showProfile, setShowProfile] = useState(false);
 
-  function profileShow(value:boolean){
-    if(value === true){
+  function profileShow(value: boolean) {
+    if (value === true) {
       setShowProfile(true);
     }
-    else{
+    else {
       setShowProfile(false);
     }
   }
@@ -31,26 +29,32 @@ export default function Header() {
           <div className="col-span-6 flex justify-center items-center relative">
             <input type="text" className="w-full rounded-3xl h-10 bg-ffbeb9 border border-white px-4 pl-10 text-white font-bold text-sm" ></input>
             <button className="absolute left-2 z-10">
-              <SearchIcon className="text-white" />
+              <IconCameraSearch size="1.7rem" stroke={1.5} className="text-white" />
             </button>
           </div>
           <div className="col-span-3 flex justify-end items-center gap-3">
             <div className="relative cursor-pointer" title="Bildirimler">
               <div className="rounded-3xl bg-e15146 border border-white p-1 py-0 absolute -top-1 -right-1 text-white text-center text-xs">2</div>
-              <NotificationsNoneIcon className="text-white" />
+              <IconBellRinging size="1.7rem" stroke={1.5} className="text-white" />
             </div>
             <div className="relative cursor-pointer" title="Mesajlar">
               <div className="rounded-3xl bg-e15146 border border-white p-1 py-0 absolute -top-1 -right-1 text-white text-center text-xs">2</div>
-              <ChatBubbleOutlineIcon className="text-white" />
+              <IconBrandHipchat size="1.7rem" stroke={1.5} className="text-white" />
             </div>
             <div className="relative cursor-pointer" title="Yeni Ekle">
-              <AddIcon className="text-white" />
+              <IconPlus size="1.7rem" stroke={1.5} className="text-white" />
             </div>
             <div className="relative cursor-pointer" title="Profilim">
-              <button onClick={() => profileShow(showProfile === false ? true : false)} className="cursor pointer">
-                <Image alt="profile" src={'/profile.jpg'} width={400} height={400} className="w-9 rounded-full border border-white" />
-              </button>
-              {showProfile && <ShowProfile profileShow={profileShow} />}
+              <Menu shadow="md" width={200} withArrow>
+                <Menu.Target>
+                  <Button className="bg-none hover:bg-transparent px-0 mx-0">
+                    <Image alt="profile" src={'/profile.jpg'} width={400} height={400} className="w-9 rounded-full border border-white" />
+                  </Button>
+                </Menu.Target>
+                <Menu.Dropdown className="py-2">
+                  <ShowProfile />
+                  </Menu.Dropdown>
+              </Menu>
             </div>
           </div>
         </div>
--- components/main/header/ShowProfile.tsx
+++ components/main/header/ShowProfile.tsx
@@ -1,37 +1,23 @@
 import Link from "next/link";
+import { Menu } from '@mantine/core';
+import { IconUserCircle, IconMoodWink, IconPackage, IconLogout2 } from '@tabler/icons-react';
 
-interface ShowProfileProps{
-  profileShow:any;
-}
-
-export default function ShowProfile({profileShow}:ShowProfileProps) {
+export default function ShowProfile() {
   return (
     <>
-      <div className="fixed w-full h-full left-0 top-0 -z-10" onClick={()=> profileShow(false)}></div>
-      <div className='absolute min-h-[100px]   right-0 bg-white mt-[2px] border border-ffbeb9 z-20' style={{ boxShadow: ' rgba(240, 113, 103, 0.4) 5px 5px, rgba(240, 113, 103, 0.3) 10px 10px, rgba(240, 113, 103, 0.2) 15px 15px, rgba(240, 113, 103, 0.1) 20px 20px, rgba(240, 113, 103, 0.05) 25px 25px' }}>
-        <ul className='py-4 px-2 w-full'>
-          <li className='text-xs w-full mb-1'>
-            <Link href={"javascript:;"} className='border-b border-dashed border-b-202124 w-full block text-202124 pb-1'>
-              Profilim
-            </Link>
-          </li>
-          <li className='text-xs w-full mb-1'>
-            <Link href={"javascript:;"} className='border-b border-dashed border-b-202124 w-full block text-202124 pb-1'>
-              Piklediklerim
-            </Link>
-          </li>
-          <li className='text-xs w-full mb-1'>
-            <Link href={"javascript:;"} className='border-b border-dashed border-b-202124 w-full block text-202124 pb-1'>
-              Koleksiyonlarım
-            </Link>
-          </li>
-          <li className='text-xs w-full'>
-            <Link href={"javascript:;"} className='w-full block text-202124 pb-1'>
-              Çıkış Yap
-            </Link>
-          </li>
-        </ul>
-      </div>
+      <Menu.Label>Merhaba @haktan</Menu.Label>
+      <Menu.Item href={"javascript:;"} component="a" icon={<IconUserCircle size={17} />}>
+        Profilim
+      </Menu.Item>
+      <Menu.Item href={"javascript:;"} component="a" icon={<IconMoodWink size={17} />}>
+        Piklediklerim
+      </Menu.Item>
+      <Menu.Item href={"javascript:;"} component="a" icon={<IconPackage size={17} />}>
+        Koleksiyonlarım
+      </Menu.Item>
+      <Menu.Item href={"javascript:;"} component="a" color="red" icon={<IconLogout2 size={17} />}>
+        Çıkış Yap
+      </Menu.Item>
     </>
   )
 }
