28ef379fade6dd6fd1f78c2db0e85d896db24a68
haktan-tekin <webhaktan@gmail.com>
2023-05-11 12:52:21 +0300

    fix: show profile links

--- components/main/header/ShowProfile.tsx
+++ components/main/header/ShowProfile.tsx
@@ -6,16 +6,16 @@ export default function ShowProfile() {
   return (
     <>
       <Menu.Label>Merhaba @haktan</Menu.Label>
-      <Menu.Item href={"javascript:;"} component="a" icon={<IconUserCircle size={17} />}>
+      <Menu.Item href={"javascript:;"} component="a" icon={<IconUserCircle size={17} stroke={1.0} />}>
         Profilim
       </Menu.Item>
-      <Menu.Item href={"javascript:;"} component="a" icon={<IconMoodWink size={17} />}>
+      <Menu.Item href={"javascript:;"} component="a" icon={<IconMoodWink size={17} stroke={1.0} />}>
         Piklediklerim
       </Menu.Item>
-      <Menu.Item href={"javascript:;"} component="a" icon={<IconPackage size={17} />}>
+      <Menu.Item href={"javascript:;"} component="a" icon={<IconPackage size={17} stroke={1.0} />}>
         Koleksiyonlarım
       </Menu.Item>
-      <Menu.Item href={"javascript:;"} component="a" color="red" icon={<IconLogout2 size={17} />}>
+      <Menu.Item href={"javascript:;"} component="a" color="red" icon={<IconLogout2 size={17} stroke={1.0} />}>
         Çıkış Yap
       </Menu.Item>
     </>
