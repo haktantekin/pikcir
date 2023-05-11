f59d7f069e871d6e3c3aff08264f83dd1b860074
haktan-tekin <webhaktan@gmail.com>
2023-05-11 12:52:35 +0300

    fix: header links

--- components/main/header/Index.tsx
+++ components/main/header/Index.tsx
@@ -1,59 +1,51 @@
 import Image from "next/image";
 import Link from "next/link";
-import { useState } from 'react';
+
+
 import ShowProfile from './ShowProfile';
 import { IconCameraSearch, IconBellRinging, IconBrandHipchat, IconPlus } from '@tabler/icons-react';
-import { Menu, Button } from '@mantine/core';
+import { Menu, Button, Modal, FileButton, Group, UnstyledButton } from '@mantine/core';
 
 export default function Header() {
-  const [showProfile, setShowProfile] = useState(false);
-
-  function profileShow(value: boolean) {
-    if (value === true) {
-      setShowProfile(true);
-    }
-    else {
-      setShowProfile(false);
-    }
-  }
-
   return (
-    <header className={`h-12 w-full bg-f07167`}>
+    <header className={`h-12 w-full bg-white border-b border-f07167`}>
       <div className="container">
         <div className="grid grid-cols-12">
           <div className="col-span-3">
             <Link href="/home" className="w-full block">
-              <Image src="/logo.png" alt="Pickup" width={384} height={114} className="w-[166px]"></Image>
+              <Image src="/logo-dark.png" alt="Pickup" width={160} height={47} className="w-[160px] h-[47px]"></Image>
             </Link>
           </div>
           <div className="col-span-6 flex justify-center items-center relative">
-            <input type="text" className="w-full rounded-3xl h-10 bg-ffbeb9 border border-white px-4 pl-10 text-white font-bold text-sm" ></input>
+            <input type="text" className="w-full rounded-3xl h-10 bg-white border border-ffbeb9 px-4 pl-10 text-ffbeb9 font-bold text-sm" ></input>
             <button className="absolute left-2 z-10">
-              <IconCameraSearch size="1.7rem" stroke={1.5} className="text-white" />
+              <IconCameraSearch size="1.7rem" stroke={1.0} className="text-f07167" />
             </button>
           </div>
           <div className="col-span-3 flex justify-end items-center gap-3">
             <div className="relative cursor-pointer" title="Bildirimler">
-              <div className="rounded-3xl bg-e15146 border border-white p-1 py-0 absolute -top-1 -right-1 text-white text-center text-xs">2</div>
-              <IconBellRinging size="1.7rem" stroke={1.5} className="text-white" />
+              <div className="rounded-3xl bg-e15146 border border-e15146 p-1 py-0 absolute -top-1 -right-1 text-white text-center text-xs">2</div>
+              <IconBellRinging size="1.7rem" stroke={1.0} className="text-343a40" />
             </div>
             <div className="relative cursor-pointer" title="Mesajlar">
-              <div className="rounded-3xl bg-e15146 border border-white p-1 py-0 absolute -top-1 -right-1 text-white text-center text-xs">2</div>
-              <IconBrandHipchat size="1.7rem" stroke={1.5} className="text-white" />
+              <div className="rounded-3xl bg-e15146 border border-e15146 p-1 py-0 absolute -top-1 -right-1 text-white text-center text-xs">2</div>
+              <IconBrandHipchat size="1.7rem" stroke={1.0} className="text-343a40" />
             </div>
             <div className="relative cursor-pointer" title="Yeni Ekle">
-              <IconPlus size="1.7rem" stroke={1.5} className="text-white" />
+              <Link href="/new-post">
+                <IconPlus size="1.7rem" stroke={1.0} className="text-f07167" />
+              </Link>
             </div>
             <div className="relative cursor-pointer" title="Profilim">
               <Menu shadow="md" width={200} withArrow>
                 <Menu.Target>
-                  <Button className="bg-none hover:bg-transparent px-0 mx-0">
+                  <UnstyledButton className="bg-none hover:bg-transparent px-0 mx-0">
                     <Image alt="profile" src={'/profile.jpg'} width={400} height={400} className="w-9 rounded-full border border-white" />
-                  </Button>
+                  </UnstyledButton>
                 </Menu.Target>
                 <Menu.Dropdown className="py-2">
                   <ShowProfile />
-                  </Menu.Dropdown>
+                </Menu.Dropdown>
               </Menu>
             </div>
           </div>
