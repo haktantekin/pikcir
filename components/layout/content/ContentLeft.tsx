ec43b7500299d286088e08197f193f4cc0792a5b
haktan-tekin <webhaktan@gmail.com>
2023-05-19 01:26:45 +0300

    remove: tag in left column

--- components/layout/content/ContentLeft.tsx
+++ components/layout/content/ContentLeft.tsx
@@ -50,40 +50,6 @@ export default function ContentLeft() {
               <Menu.Item href={"/home"} component="a" icon={<IconAwardFilled size={25} stroke={0.5} />} className="text-sm">
                 Aylık Yarışmalar
               </Menu.Item>
-              <Menu.Divider />
-              <Menu.Label>Günün Etiketleri</Menu.Label>
-              <div className={"grid grid-cols-2"}>
-                <Menu.Item href={"/kategori/serbest"} component="a" icon={<IconHash size={15} stroke={0.5} className="!-mr-2" />} className="text-sm pb-0">
-                  Serbest
-                </Menu.Item>
-                <Menu.Item href={"/home"} component="a" icon={<IconHash size={15} stroke={0.5} className="!-mr-2" />} className="text-sm pb-0">
-                  Dizi/Film
-                </Menu.Item>
-                <Menu.Item href={"/home"} component="a" icon={<IconHash size={15} stroke={0.5} className="!-mr-2" />} className="text-sm pb-0">
-                  İlişki
-                </Menu.Item>
-                <Menu.Item href={"/home"} component="a" icon={<IconHash size={15} stroke={0.5} className="!-mr-2" />} className="text-sm pb-0">
-                  Eğitim
-                </Menu.Item>
-                <Menu.Item href={"/home"} component="a" icon={<IconHash size={15} stroke={0.5} className="!-mr-2" />} className="text-sm pb-0">
-                  Siyasi
-                </Menu.Item>
-                <Menu.Item href={"/home"} component="a" icon={<IconHash size={15} stroke={0.5} className="!-mr-2" />} className="text-sm pb-0">
-                  Yetişkin
-                </Menu.Item>
-                <Menu.Item href={"/home"} component="a" icon={<IconHash size={15} stroke={0.5} className="!-mr-2" />} className="text-sm pb-0">
-                  Sanat
-                </Menu.Item>
-                <Menu.Item href={"/home"} component="a" icon={<IconHash size={15} stroke={0.5} className="!-mr-2" />} className="text-sm pb-0">
-                  Troll
-                </Menu.Item>
-                <Menu.Item href={"/home"} component="a" icon={<IconHash size={15} stroke={0.5} className="!-mr-2" />} className="text-sm pb-0">
-                  Tweet
-                </Menu.Item>
-                <Menu.Item href={"/home"} component="a" icon={<IconHash size={15} stroke={0.5} className="!-mr-2" />} className="text-sm">
-                  Karikatür
-                </Menu.Item>
-              </div>
             </Menu>
           </div>
         </div>
