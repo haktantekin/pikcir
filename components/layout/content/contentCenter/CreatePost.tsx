f90d82d0a970c765f29ba8279bc67ce7f0807dc7
haktan-tekin <webhaktan@gmail.com>
2023-05-19 01:29:47 +0300

    fix: create post space

--- components/layout/content/contentCenter/CreatePost.tsx
+++ components/layout/content/contentCenter/CreatePost.tsx
@@ -14,32 +14,32 @@ export default function CreatePost() {
   };
   return (
     <>
-        <div className='flex flex-col'>
-          {file && (
-            <div className="mx-auto w-full relative">
-              <div className={`${!file ? 'hidden' : 'flex justify-center items-center'} mx-aut absolute h-4 w-4 bg-e15146 text-white rounded-full inline-flex left-2 top-2 justify-center items-center text-base cursor-pointer p-0 mt-0`}>
-                <UnstyledButton className="" disabled={!file} onClick={clearFile} classNames={"leading-0"}>
-                  <span className="text-white relative -top-px">&times;</span>
-                </UnstyledButton>
-              </div>
-              <Image className="max-w-full w-full" width={300} height={200} src={`/${file.name}`} alt="" />
+      <div className='flex flex-col'>
+        {file && (
+          <div className="mx-auto w-full relative">
+            <div className={`${!file ? 'hidden' : 'flex justify-center items-center'} mx-aut absolute h-4 w-4 bg-e15146 text-white rounded-full inline-flex left-2 top-2 justify-center items-center text-base cursor-pointer p-0 mt-0`}>
+              <UnstyledButton className="" disabled={!file} onClick={clearFile} classNames={"leading-0"}>
+                <span className="text-white relative -top-px">&times;</span>
+              </UnstyledButton>
             </div>
-          )}
-          <div className='flex gap-4 w-full'>
-            <div className={`${!file ? 'block' : 'hidden'} w-full`}>
-              <FileButton resetRef={resetRef} onChange={setFile} accept="image/png,image/jpeg">
-                {(props) => <Button className="border border-e15146 text-e15146 hover:bg-e15146 hover:text-white w-full bg-white" {...props}>İçerik Gönder</Button>}
-              </FileButton>
-            </div>
+            <Image className="max-w-full w-full" width={300} height={200} src={`/${file.name}`} alt="" />
           </div>
-          <div className='flex justify-center items-center flex-col gap-4 w-full  mt-4'>
-            {file && (
-              <>
-                <TagsInput />
-              </>
-            )}
+        )}
+        <div className='flex gap-4 w-full'>
+          <div className={`${!file ? 'block' : 'hidden'} w-full`}>
+            <FileButton resetRef={resetRef} onChange={setFile} accept="image/png,image/jpeg">
+              {(props) => <Button className="border border-e15146 text-e15146 hover:bg-e15146 hover:text-white w-full bg-white" {...props}>İçerik Gönder</Button>}
+            </FileButton>
           </div>
         </div>
+        {file && (
+          <>
+            <div className='flex justify-center items-center flex-col gap-4 w-full  mt-4'>
+              <TagsInput />
+            </div>
+          </>
+        )}
+      </div>
 
     </>
   )
