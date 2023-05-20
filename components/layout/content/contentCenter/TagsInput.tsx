c3ad225687ae9db920f8fdf9f269267759ce4a7d
haktan-tekin <webhaktan@gmail.com>
2023-05-20 14:32:00 +0300

    change: input type

--- components/layout/content/contentCenter/TagsInput.tsx
+++ components/layout/content/contentCenter/TagsInput.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { UnstyledButton, Input, Tooltip } from '@mantine/core';
+import { UnstyledButton, Textarea, Tooltip } from '@mantine/core';
 import { IconBrandTwitter, IconAlertCircle, IconInfoSmall, IconAlignJustified } from '@tabler/icons-react';
 
 export default function TagsInput() {
@@ -32,8 +32,7 @@ export default function TagsInput() {
     <>
       <div className='grid grid-cols-12 w-full gap-2'>
         <div className='col-span-12 post-title'>
-          <Input
-            icon={<IconAlignJustified size="1rem" />}
+          <Textarea
             className=''
             placeholder="Pikçır'ın esprisi nedir?"
             maxLength={80}
@@ -46,7 +45,7 @@ export default function TagsInput() {
             }
           />
         </div>
-        <div className='col-span-10'>
+        <div className='col-span-12'>
           <div className='flex flex-col w-full bg-white border border-e15146 rounded p-2'>
             <div className="w-full flex items-center flex-wrap gap-2">
               {tags.map((tag: any, index: number) => (
@@ -60,13 +59,13 @@ export default function TagsInput() {
                   onKeyDown={handleKeyDown}
                   type="text"
                   className="tags-input border-none outline-none bg-transparent text-xs w-auto flex-grow min-h-[35px] placeholder:text-202124 text-202124 font-bold"
-                  placeholder={tags.length !== 0 ? 5 - tags.length + " tag daha ekleyebilirsin" : '5 tag ekleyebilirsin'} />
+                  placeholder={tags.length !== 0 ? 5 - tags.length + " tag daha ekle" : '5 tag ekle'} />
               }
             </div>
           </div>
         </div>
-        <div className='col-span-2 my-auto min-h-[53px]'>
-          <UnstyledButton className={`w-full h-full flex text-center justify-center items-center rounded font-bold text-white text-base min-h-[53px] ${tags.length <= 0 ? 'bg-343a40 pointer-events-none' : 'bg-e15146'}`} disabled={tags.length < 0}>Pikle!</UnstyledButton>
+        <div className='col-span-12 my-auto'>
+          <UnstyledButton className={`w-full h-full flex text-center justify-center items-center rounded font-bold text-white text-base max-w-[100px] mx-auto min-h-[40px] ${tags.length <= 0 ? 'bg-343a40 pointer-events-none' : 'bg-e15146'}`} disabled={tags.length < 0}>Pikle!</UnstyledButton>
         </div>
       </div>
     </>
