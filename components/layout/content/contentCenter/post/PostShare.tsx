2d0d972c19e072a9d44103fb9a5a0f377a2afc29
haktan-tekin <webhaktan@gmail.com>
2023-05-20 14:51:35 +0300

    add: Pinterest Share

--- components/layout/content/contentCenter/post/PostShare.tsx
+++ components/layout/content/contentCenter/post/PostShare.tsx
@@ -1,5 +1,5 @@
 import { Menu, Button, Text } from '@mantine/core';
-import { IconSettings, IconSearch, IconPhoto, IconMessageCircle, IconTrash, IconArrowsLeftRight, IconShare3, IconBrandFacebook, IconBrandTwitter, IconBrandWhatsapp, IconLink } from '@tabler/icons-react';
+import { IconSettings, IconSearch, IconPhoto, IconMessageCircle, IconTrash, IconArrowsLeftRight, IconShare3, IconBrandFacebook, IconBrandTwitter, IconBrandWhatsapp, IconLink, IconBrandPinterest } from '@tabler/icons-react';
 import Link from 'next/link';
 
 
@@ -11,10 +11,11 @@ export default function PostShare() {
           <Link href="javascript:;" className="flex items-center gap-1 text-sm"><IconShare3 size={18} />Paylaş</Link>
         </Menu.Target>
         <Menu.Dropdown>
-          <Menu.Item icon={<IconBrandFacebook size={18} />}>Facebook</Menu.Item>
-          <Menu.Item icon={<IconBrandTwitter size={18} />}>Twitter</Menu.Item>
-          <Menu.Item icon={<IconBrandWhatsapp size={18} />}>Whatsapp</Menu.Item>
           <Menu.Item icon={<IconLink size={18} />}>Linki Kopyala</Menu.Item>
+          <Menu.Item icon={<IconBrandWhatsapp size={18} />}>Whatsapp</Menu.Item>
+          <Menu.Item icon={<IconBrandTwitter size={18} />}>Twitter</Menu.Item>
+          <Menu.Item icon={<IconBrandFacebook size={18} />}>Facebook</Menu.Item>
+          <Menu.Item icon={<IconBrandPinterest size={18} />}>Pinterest</Menu.Item>
         </Menu.Dropdown>
       </Menu>
     </>
