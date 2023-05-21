f1f38fe6cb86b337d9273bf74e7d9e0b043e7c00
haktan-tekin <webhaktan@gmail.com>
2023-05-21 14:32:04 +0300

    add: share new link

--- components/layout/content/contentCenter/post/PostShare.tsx
+++ components/layout/content/contentCenter/post/PostShare.tsx
@@ -1,5 +1,5 @@
 import { Menu } from '@mantine/core';
-import { IconShare3, IconBrandFacebook, IconBrandTwitter, IconBrandWhatsapp, IconLink, IconBrandPinterest, IconDownload } from '@tabler/icons-react';
+import { IconShare3, IconBrandFacebook, IconBrandTwitter, IconBrandWhatsapp, IconLink, IconBrandPinterest, IconDownload, IconSticker } from '@tabler/icons-react';
 import Link from 'next/link';
 
 export default function PostShare() {
@@ -12,7 +12,8 @@ export default function PostShare() {
         <Menu.Dropdown>
           <Menu.Item icon={<IconLink size={18} />}>Linki Kopyala</Menu.Item>
           <Menu.Item icon={<IconDownload size={18} />}>Resmi İndir</Menu.Item>
-          <Menu.Item icon={<IconBrandWhatsapp size={18} />}>Whatsapp</Menu.Item>
+          <Menu.Item icon={<IconBrandWhatsapp size={18} />}>Whatsapp&apos;a Gönder</Menu.Item>
+          <Menu.Item icon={<IconSticker size={18} />}>Sticker Yap</Menu.Item>
           <Menu.Item icon={<IconBrandTwitter size={18} />}>Twitter</Menu.Item>
           <Menu.Item icon={<IconBrandFacebook size={18} />}>Facebook</Menu.Item>
           <Menu.Item icon={<IconBrandPinterest size={18} />}>Pinterest</Menu.Item>
