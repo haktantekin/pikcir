0c5badc84dbf7a12099049a74f0d616376e491cc
haktan-tekin <webhaktan@gmail.com>
2023-05-27 00:52:02 +0300

    change: search icon

--- components/main/Search.tsx
+++ components/main/Search.tsx
@@ -1,11 +1,11 @@
-import { IconCameraSearch } from "@tabler/icons-react";
+import { IconMoodSearch } from "@tabler/icons-react";
 
 export default function Search() {
   return (
     <>
       <input type="text" className="w-full rounded-3xl h-10 bg-white border border-e15146 px-4 pl-10 text-e15146 font-bold text-sm" ></input>
       <button className="absolute left-2 z-10">
-        <IconCameraSearch size="1.7rem" stroke={1.0} className="text-f07167" />
+        <IconMoodSearch size="1.7rem" stroke={1.5} className="text-f07167" />
       </button>
     </>
   )
